29cca3fef813e80c65ff726c9b7b73e261a66c7d
Kevin Wilson <104521683+KevinWilsonAT@users.noreply.github.com>
2024-01-13 15:10:12 -0300

    Challenges #6
    6th Folder Of Challenges Using Python Programming Language

---
+++ Mundo 1 - Fundamentos/CursoPythonGuanabara10-desafios/aula10-desafio28.py
@@ -0,0 +1,11 @@
+from random import randint
+
+chosen = randint(0, 5)
+
+user = int(input('Insert a number: '))
+
+if user == chosen:
+    print('Processing...')
+    print('Congrats! You discovered the hidden number: {}'.format(chosen))
+else:
+    print('Too bad! The hidden number was {}' .format(chosen))
