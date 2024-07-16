5d93383e43abbf5f6383d8e4c5b8fb12f60e73f5
Kevin Wilson <104521683+KevinWilsonAT@users.noreply.github.com>
2024-07-15 22:14:02 -0300

    Chore: upload class 04-07-24

--- Mundo 3 - Estruturas Compostas/CursoPythonGuanabara22-desafios/aula22-desafio107.py
+++ Mundo 3 - Estruturas Compostas/CursoPythonGuanabara22-desafios/aula22-desafio107.py
@@ -1,14 +1,9 @@
-# - criar modulo chamado coin.py
-#       - coloque funcoes incorporadas augm(), red(), double(), half()
-#       - fazer um programa que importe esse modulo e use algumas dessas funcoes
-
 # MAIN PROGRAM
 
-from desafio107_module import coin
+import coin_desafio107_module as coin
 
 p = float(input('Insert the price: R$'))
-
 print(f'The half of the value {p} is: {coin.half(p)}')
 print(f'The double of the value {p} is: {coin.double(p)}')
-print(f'Augmenting 10% to the value {p}, we have: {coin.augm(p)}')
-print(f'Reducing 13% of the value {p}, we have: {coin.red(p)}')
+print(f'Augmenting 10% to the value {p}, we have: {coin.augm(p, 10)}')
+print(f'Reducing 10% of the value {p}, we have: {coin.red(p, 10)}')
