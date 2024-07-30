1a500daa175f1a4670a69410d743b7bbcc23d40d
Kevin Wilson <104521683+KevinWilsonAT@users.noreply.github.com>
2024-07-30 17:45:00 -0300

    Chore: upload class 20-07-24

--- Mundo 3 - Estruturas Compostas/CursoPythonGuanabara22-desafios/aula22-desafio111.py
+++ Mundo 3 - Estruturas Compostas/CursoPythonGuanabara22-desafios/aula22-desafio111.py
@@ -1,3 +1,7 @@
-# crie um pacote chamado utilsCeV que tenha 2 modulos internos chamados coin e data (datas, se der erro).
-# transfira todas as funcoes utilizadas nos desafios 107, 108, e 109 para o primeiro pacote e mantenha tudo
-# funcionando 
+# MAIN PROGRAM
+
+from utilscev import coin
+from utilscev import datas
+
+p = datas.readMoney('Insert the price: R$')
+coin.stats(p, 20, 12)
